c9bb26043d6503ff108435e7c6e54a0bef9d0904
Tim Watel <watel.timothee@gmail.com>
2019-10-16 16:25:10 +0200

    First commit

---
+++ includes/tables/PostTable.php
@@ -0,0 +1,64 @@
+<?php
+
+class PostTable
+{
+    protected $table = 'posts';
+    private $db;
+
+    public function __construct()
+    {
+        global $db;
+        $this->db = $db;
+    }
+
+    public function get(int $id): Post
+    {
+        $sth = $this->db->prepare("SELECT * FROM {$this->table} WHERE id = :id");
+        $sth->bindParam(':id', $id);
+        $result = $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    public function all(): array
+    {
+        $sth = $this->db->query("SELECT * FROM {$this->table}");
+        return $sth->fetchAll();
+    }
+
+    public function create(Post $post): void
+    {
+        $sth = $this->db->prepare("INSERT INTO {$this->table} (title, content) VALUES (:title, :content)");
+        $title = $post->getTitle();
+        $content = $post->getContent();
+        $sth->bindParam(':title', $title);
+        $sth->bindParam(':content', $content);
+        $result = $sth->execute();
+
+        if (!$result) {
+            throw new Exception("Error during creation with the table {$this->table}");
+        }
+    }
+
+    public function update(Post $post): void
+    {
+        $sth = $this->db->prepare("UPDATE {$this->table} (title, content) VALUES (:title, :content)");
+        $sth->bindParam(':title', $post->getTitle());
+        $sth->bindParam(':content', $post->getContent());
+        $result = $sth->execute();
+
+        if (!$result) {
+            throw new Exception("Error during update with the table {$this->table}");
+          }
+    }
+
+    public function delete(int $id): void
+    {
+      $sth = $this->db->prepare("DELETE {$this->table} WHERE id = :id");
+      $sth->bindParam(':id', $id);
+      $result = $sth->execute();
+
+      if (!$result) {
+          throw new Exception("Error during delete with the table {$this->table}");
+        }
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once '../includes/config.php';
+
+$pt = new PostTable();
+
+if (isset($_POST['title']) && isset($_POST['content'])) {
+    $post = new Post();
+    $post->setTitle($_POST['title']);
+    $post->setContent($_POST['content']);
+    $pt->create($post);
+}
+
+$posts = $pt->all();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">BLOG RAP</h1>
+        <div class="row">          
+           <?php foreach($posts as $post): ?>
+                <div class="col-md-4">
+                    <h2><?= $post['title'] ?></h2>
+                    <p><?= $post['content'] ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <br></br>
+        <h1>Ajouter un poste</h1>
+        <div class="row">
+            <form action="" method="POST">
+                <div class="form-group">
+                    <label for="title">Titre</label>
+                    <input type="text" class="form-control" name="title">
+                </div>
+                <div class="form-group">
+                    <label for="title">Contenu</label>
+                    <textarea class="form-control" name="content"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
